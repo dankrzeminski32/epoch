478c0d023a4911a2b9a6b9e55d3cebe2817cf5fa
dankrzeminski32 <dankrzeminski32@gmail.com>
2023-11-21 12:59:21 -0600

    login required for protected views

--- epoch/sources/views.py
+++ epoch/sources/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import redirect
 from django.contrib import messages  # import messages
 from datetime import datetime, timedelta
 from .filters import HeadlineFilter
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def sources(request):
     current_user = request.user
     sources: list[Source] = Source.objects.all()
@@ -14,23 +16,23 @@ def sources(request):
     return render(request, "sources/sources.html", {"sources": sources, "my_sources": my_sources})
 
 
+@login_required
 def news(request):
     current_user = request.user
     users_subscribed_sources: list[Source] = Source.objects.all().filter(
         subscribers__in=[current_user.pk]).values_list("id", flat=True)
     qry = Headline.objects.all().filter(source_id__in=users_subscribed_sources)
     f = HeadlineFilter(request.GET, queryset=qry, request=request)
-    print(f.qs.query)
-
     return render(request, "sources/news.html", {"filter": f})
 
 
+@ login_required
 def news_detail(request, headline_id):
     requested_headline = Headline.objects.get(pk=headline_id)
-    print(requested_headline)
     return render(request, "sources/news-detail.html", {"headline": requested_headline})
 
 
+@ login_required
 def add_source(request, source_id):
     current_user = request.user
     source_added: Source = Source.objects.get(pk=source_id)
@@ -39,6 +41,7 @@ def add_source(request, source_id):
     return redirect(sources)
 
 
+@ login_required
 def delete_source(request, source_id):
     current_user = request.user
     source_added: Source = Source.objects.get(pk=source_id)
